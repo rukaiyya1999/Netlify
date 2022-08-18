db7067ce683183e661f727a05a5ecad0659faee1
rukaiyya <nandrekarruk@gmail.com>
2022-08-19 00:26:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This is React
         </a>
       </header>
     </div>
